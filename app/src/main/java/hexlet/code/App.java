462add8df6a0fd2dc1ba27c1e3c427722562ad78
areldin8 <belphegor26ra@gmail.com>
2024-03-11 22:00:23 +0300

    App add

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,51 @@
 package hexlet.code;
+import java.util.Scanner;
+import static hexlet.code.Cli.greeting;
+import static hexlet.code.games.Even.gameEven;
+import static hexlet.code.games.Calc.gameCalc;
+import static hexlet.code.games.Node.gameNode;
+import static hexlet.code.games.Prime.gamePrime;
+import static hexlet.code.games.Progression.gameProgression;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+                var game = 0;
+                System.out.println(
+                        "Please enter the game number and press Enter\n"
+                                +"1 - Greet\n"
+                                +"2 - Even\n"
+                                +"3 - Calc\n"
+                                +"4 - GCD\n"
+                                +"5 - Progression\n"
+                                +"6 - Prime\n"
+                                +"0 - Exit\n"
+                                +"Your choice: "
+                );
+                game = scanner.nextInt();
+        switch (game){
+            case 0:
+                System.exit(1);
+                break;
+            case 1:
+                greeting();
+                break;
+            case 2:
+                gameEven();
+                break;
+            case 3:
+                gameCalc();
+            case 4:
+                gameNode();
+            case 5:
+                gameProgression();
+            case 6:
+                gamePrime();
+            default:
+                System.out.println("Undefined input. Please choose number from 0 to 6.");
+        }
+        scanner.close();
     }
 }
+
+

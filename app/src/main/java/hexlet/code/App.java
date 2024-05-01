faa18d6b5532ef94cd3e5d22366b3a4f6d59bd60
areldin <belphegor26ra@gmail.com>
2024-05-01 23:59:19 +0300

    updated

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,46 +1,49 @@
 package hexlet.code;
 import java.util.Scanner;
 import static hexlet.code.Cli.greeting;
-import static hexlet.code.games.Even.gameEven;
-import static hexlet.code.games.Calc.gameCalc;
-import static hexlet.code.games.Node.gameNode;
-import static hexlet.code.games.Prime.gamePrime;
-import static hexlet.code.games.Progression.gameProgression;
+import static hexlet.code.games.Even.startEven;
+import static hexlet.code.games.Calc.startCalc;
+import static hexlet.code.games.Node.startNode;
+import static hexlet.code.games.Prime.startPrime;
+import static hexlet.code.games.Progression.startProgression;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-                var game = 0;
-                System.out.println(
+        var game = 0;
+        System.out.println(
                         "Please enter the game number and press Enter\n"
-                                +"1 - Greet\n"
-                                +"2 - Even\n"
-                                +"3 - Calc\n"
-                                +"4 - GCD\n"
-                                +"5 - Progression\n"
-                                +"6 - Prime\n"
-                                +"0 - Exit\n"
-                                +"Your choice: "
-                );
-                game = scanner.nextInt();
-        switch (game){
+                                + "1 - Greet\n"
+                                + "2 - Even\n"
+                                + "3 - Calc\n"
+                                + "4 - GCD\n"
+                                + "5 - Progression\n"
+                                + "6 - Prime\n"
+                                + "0 - Exit\n"
+                                + "Your choice: "
+        );
+        game = scanner.nextInt();
+        switch (game) {
             case 0:
-                System.exit(1);
-                break;
+                return;
             case 1:
                 greeting();
                 break;
             case 2:
-                gameEven();
+                startEven();
                 break;
             case 3:
-                gameCalc();
+                startCalc();
+                break;
             case 4:
-                gameNode();
+                startNode();
+                break;
             case 5:
-                gameProgression();
+                startProgression();
+                break;
             case 6:
-                gamePrime();
+                startPrime();
+                break;
             default:
                 System.out.println("Undefined input. Please choose number from 0 to 6.");
         }

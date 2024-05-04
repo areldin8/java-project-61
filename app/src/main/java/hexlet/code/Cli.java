4a1b714d41870ddc745c79c03fb67a1de0abf20c
areldin <belphegor26ra@gmail.com>
2024-05-04 15:47:40 +0300

    fixed

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,7 +7,7 @@ public class Cli {
         System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
-        System.out.println("Hello, " + username + "!");
+        System.out.println("Hello,"+ " " + username + "!");
         return username;
     }
 }

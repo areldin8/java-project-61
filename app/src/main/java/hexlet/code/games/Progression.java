a314cd69d05ffec985f9cb7b681913f7a7b62abd
areldin <belphegor26ra@gmail.com>
2024-05-12 17:49:53 +0300

    check 4

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,6 +4,8 @@ import hexlet.code.Utils;
 
 public class Progression {
     public static final String DESCRIPTION = "What number is missing in the progression?";
+    public static final int MAX_SIZE = 10;
+    public static final int MIN_SIZE = 5;
 
     public static void startProgression() {
         var questionsAndAnswers = new String[Engine.ROUNDS_COUNT][];
@@ -15,7 +17,7 @@ public class Progression {
 
     public static String[] generateRound() {
         var questionsAndAnswers = new String[2];
-        int numbersInRow = Utils.getRandomSizeProgression();
+        int numbersInRow = Utils.getRandomNumber(MAX_SIZE, MIN_SIZE);
         int missingNumberIndex = Utils.getRandomNumber(numbersInRow);
         int increment = Utils.randomizer();
         int currentNum = Utils.randomizer();
